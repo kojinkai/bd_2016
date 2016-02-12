08ffb29d3d0c078ee1a7b6406ed03363d63d8257
Lewis Nixon <lewcfn@gmail.com>
2016-02-12 10:32:52 +0000

    break down tasks into own files and add config

---
+++ gulp/tasks/develop.js
@@ -0,0 +1,21 @@
+var gulp       = require('gulp');
+var nodemon    = require('gulp-nodemon');
+var livereload = require('gulp-livereload');
+
+gulp.task('develop', function () {
+  livereload.listen();
+  nodemon({
+    script: 'bin/www',
+    ext: 'js jade coffee',
+    stdout: false
+  })
+  .on('readable', function () {
+    this.stdout.on('data', function (chunk) {
+      if(/^Express server listening on port/.test(chunk)){
+        livereload.changed(__dirname);
+      }
+    });
+    this.stdout.pipe(process.stdout);
+    this.stderr.pipe(process.stderr);
+  });
+});
